0789819b08b89a60db9d7d3407ca5b38cd94af60
ArthurInfo123 <102191388+ArthurInfo123@users.noreply.github.com>
2022-04-03 21:17:57 -0300

    Add files via upload

---
+++ Javs.js
@@ -0,0 +1,42 @@
+function cadastrar()     
+{
+      const usuarios = []
+      let totusuarios = 0      
+      let nome = document.getElementById('nome').value
+      let Entrada = document.getElementById('Data de entrada').value
+      let cpf = document.getElementById('CPF').value
+      let telefone = document.getElementById('Telefone').value  
+      let res = document.getElementById('res')  
+      let corpo = document.getElementById('corpo')
+      res.innerHTML = ''
+      let u = {nomes:nome, entradas:Entrada, cpfs:cpf, telefones:telefone}
+      usuarios[totusuarios] = u      
+      //let td1 = document.createElement('td')
+      //let td2 = document.createElement('td')
+      //let td3 = document.createElement('td')
+      //let td4 = document.createElement('td')
+      //let br = document.createElement('br')
+      //td1.textContent = nome     
+      //td2.textContent = telefone 
+      //td3.textContent = Entrada
+     // td4.textContent = cpf
+      //corpo.appendChild(td1)     
+     // corpo.appendChild(td2)     
+      //corpo.appendChild(td3)      
+      //corpo.appendChild(td4)
+
+      var row = corpo.insertRow(-1)      
+      var cel1 = row.insertCell(-1)
+      var cel2 = row.insertCell()
+      var cel3 = row.insertCell()
+      var cel4 = row.insertCell()
+      cel1.innerHTML = nome
+      cel2.innerHTML = telefone
+      cel3.innerHTML = Entrada
+      cel4.innerHTML = cpf
+      
+
+      
+      totusuarios ++
+
+}
