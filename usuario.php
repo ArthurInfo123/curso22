6a7b8d859906980422e0fadd9dfd75599e15cc6a
Arthur Trevisan <ArthurInfoserv@gmail.com>
2022-04-12 22:05:58 -0300

    Variaveis do php

--- usuario.php
+++ usuario.php
@@ -1,5 +1,44 @@
-<?php
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <form>
+        <input type="Number" name="num">
+        <input type="submit" name="" value="Enviar">
+        </form>
+        <?php                       
+           echo "{$_REQUEST['num']}<br><br>";
+            if($_REQUEST['num']%2 == 0){
+                echo "É par";
+                
 
-echo "Sou um usuario";
+             }
+             else{
+                echo "É impar";
+            }
 
-?>
+           $soma = 0;
+           $valor1 = 10;
+           $valor2 = 20;
+           $soma = $valor1 + $valor2;
+           $soma += 29;
+           echo "<br><br>A soma dos numeros {$valor1} {$valor2} é: {$soma}";          
+           $soma += 29;
+           $palavra = "info";
+           $palavra = "serv";
+
+           $array = [10];
+           for($a = 0; $a < 10; $a++)
+           {
+                echo "$array[$a]";
+           };
+
+ 
+        ?>  
+    </body>
+</html>
+
+
+
+
