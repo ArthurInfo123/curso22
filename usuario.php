5d91a4c5dafdfdbb318655e9868dc9a997811d24
Arthur Trevisan <ArthurInfoserv@gmail.com>
2022-04-19 21:44:59 -0300

    Aula 3 php

--- usuario.php
+++ usuario.php
@@ -7,17 +7,101 @@
         <input type="Number" name="num">
         <input type="submit" name="" value="Enviar">
         </form>
-        <?php                       
-           echo "{$_REQUEST['num']}<br><br>";
-            if($_REQUEST['num']%2 == 0){
-                echo "É par";
-                
+        <?php 
+        $resto = $_REQUEST['num'] % 2; 
+        $array = [
+            0 => "é par",
+            1 => "é impar",
+        ];
+        
+        echo $array[$resto] ?? " Nao foi possivel calcular ";
+        $numero = [4,2,5,1,10,100,50,3,9,12];
 
-             }
+        $contPares = 0;
+        $i = 0;
+
+        while ($contPares <= 5){
+            $resto = $numero[$i] %2;
+            
+            if($resto == 0){
+                $contPares++;
+            }            
+            $i++;           
+        }
+        echo "{$i} {$contPares}";        
+       
+    
+            if($resto == 0 && $_REQUEST['num'] == 2){
+                echo "É par e 2";               
+
+             }             
              else{
                 echo "É impar";
             }
 
+        for($i = 0; $i < 5; $i++){            
+            $resto = $numeros[$i];            
+            if($resto == 0){
+                $contPares += 1;
+            }            
+        }    
+        echo " dolar i{$i}";
+
+        function contadorParesfor($num)
+        {
+              $contPar = 0;
+              for($i = 0; $i < 5; $i++)
+              {
+                  $resto = $num[$i] % 2;
+                  if($resto == 0){
+                      $contPar += 1;
+                  }                 
+              }
+              echo " Quantidade de pares {$contPar}";
+        }
+
+        function contadorParesWhile($num)
+        {
+            $i = 0;
+            $contPar = 0;
+            while($contPar < 5)
+            {
+                $resto = $num[$i] % 2;
+                if($resto == 0){
+                    $contPar += 1;
+                }
+                $i++;
+            }
+            echo " Quantidade de pares{$i}";
+        }
+
+        function contadorParesDo($num)
+        {   
+            $contPar = 0;
+            $i = 0;
+           do{              
+                $resto = $num[$i] % 2;
+                if($resto == 0)  
+                 {
+                     $contPar += 1;
+                 }
+                 $i++;               
+           }while($contPar < 5);          
+           echo "Quantidade de pares {$i}"; 
+        }
+
+        function DataHora($num){           
+           echo date('l jS \of F Y h:i:s A');
+           echo "<br><br>";
+        }
+        echo "<br><br>";
+        contadorParesfor($array);
+        echo "<br><br>";
+        contadorParesWhile($array);
+        echo "<br><br>";     
+        contadorParesDo($array); 
+        echo "<br><br>";
+        DataHora($array);
            $soma = 0;
            $valor1 = 10;
            $valor2 = 20;
@@ -28,11 +112,13 @@
            $palavra = "info";
            $palavra = "serv";
 
-           $array = [10];
-           for($a = 0; $a < 10; $a++)
+           switch($_REQUEST['num'] % 2)
            {
-                echo "$array[$a]";
-           };
+                case 0: echo "O numero é par";
+                break;
+                default: echo "O numero é impar";
+                break; 
+           }
 
  
         ?>  
