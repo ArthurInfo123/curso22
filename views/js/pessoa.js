fc71c15946b59c74fb76798aa3a5acef7337053f
Arthur Trevisan <ArthurInfoserv@gmail.com>
2022-05-31 22:07:46 -0300

    aula 7

---
+++ views/js/pessoa.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+      
+
+    $(".btnEditar").on("click", function(){
+           var id = $(this).val();
+           console.log("Sou o clicar do editar id:" + id);
+
+
+    });
+
+    $(".btnExcluir").on("click", function(){
+        console.log("Sou o clicar do excluir");
+
+
+ });
+
+});
--- views/siteResp.php
+++ views/siteResp.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./js/pessoa.js"></script>
     <title>Site Responsivo</title>
     <style>
         section{            
@@ -62,22 +66,30 @@
                 
         }
     </style>
+    <!-- <script src="scripts.js"></script> -->
+
 </head>
 <body>
-    <form>
-        <h1> Cadastros </h1>
-       <section>
-       <p>
-           
 
-        </p>
-       </section>
-       <section class="img">           
-          <p>Email <label><input type="email" id="email" name="email" required placeholder="E-mail"></label></p>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        Cadastrar
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Cadastro</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div>
+            <p>Email <label><input type="email" id="email" name="email" required placeholder="E-mail"></label></p>
           <p>Nome <label><input type="text" id="nome" name="nome" required placeholder="Nome"></label>
-          <p><label>Cep <input type="number" id="cep" name="cep" required placeholder="Cep"></label></p>
-          <p><label>Telefone <input type="tel" id="tel" name="tel" required placeholder="Telefone"></p>
-          <p>Cpf <label><input type="number" id="cpf" name="cpf" required placeholder="cpf"></label></p>
+          <p><label>Cep <input oninput='mask_cep(this)' type="text" id="cep" name="cep" required placeholder="Cep"></label></p>
+          <p><label>Telefone <input onkeypress="mask_phone(this, mphone)"  type="tel" id="tel" name="tel" required placeholder="Telefone" ></p>
+          <p>Cpf <label><input oninput="mask_cpf(this)" type="text" id="cpf" name="cpf" required placeholder="cpf"></label></p>
           <p>Rg <label><input type="number" id="rg" name="rg" required placeholder="Rg"></label></p>
          
             Escolaridade <select>
@@ -98,15 +110,75 @@
               </option>
           </select>
           <p>Profissao <label><input type="text" id="profissao" name="profissao" required placeholder="profissao"></label></p>
-          <p>Cnpj <label><input type="number" id="cnpj" name="cnpj" required placeholder="Cnpj"></label></p>
+          <p>Cnpj <label><input type="text" id="cnpj" name="cnpj" required placeholder="Cnpj"></label></p>
           <p>Nome da Empresa <label><input type="text" id='Nempresa' name="Nempresa"></label></p>
        </section> 
        <div>
            <input type="submit">
        </div> 
+
+            </div>
+
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            <button type="button" class="btn btn-primary">Salvar</button>
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
+                <table class="table table-dark table-striped">
+                <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            </tr>
+            <th scope="row"></th>
+            <td>
+                <button class='btnEditar' value="1"> EDITAR</button>
+                <button class='btnExcluir' value="1"> EXCLUIR </button></td>
+            
+        </tbody>
+                        </table>
+                </div>
+            </div>
+            </div>
+
+        <form>
+
+        <h1> Cadastros </h1>
+       <section>
+       <p>
+           
+
+        </p>
+       </section>
+      
     </form>
       
-
+     
 
 
 </body>
