eeb887c96e9f780e0c46d14896799caab1129157
Arthur Trevisan <ArthurInfoserv@gmail.com>
2022-06-14 22:05:04 -0300

    aula 8

--- views/js/pessoa.js
+++ views/js/pessoa.js
@@ -1,14 +1,19 @@
 $( document ).ready(function() {
       var baseUrl = "http://localhost/curso22/controlers/pessoacontroler.php"
 
-    $(".btnEditar").on("click", function(){
+    $(".btnSalvar").on("click", function(){
            var idPessoa = $(this).val();
            baseUrl += "?id=" + idPessoa
            $.get(baseUrl).done(function (response)
            {
+                  //html --> back
                   $('#nome').val(response[0].nome)
+                  $('#email').val(response[0].email)
+                  $("#tel").val(response[0].phone)
+                  $("#cep").val(response[0].cep)
+                  $("#sexo").val(response[0].gender)
                   console.log(response)
-                  $('.modal fade').modal('show')
+                  $('.modal').modal('show')
 
                   console.log(JSON.stringify(response))
            })
@@ -17,10 +22,63 @@ $( document ).ready(function() {
 
     });
 
-    $(".btnExcluir").on("click", function(){
+    $("#btnExcluir").on("click", function(){
         console.log("Sou o clicar do excluir");
 
+   
+       
 
  });
 
+    function montarCabecalho()
+    {
+           var html = (
+                  `
+                  <thead>
+                  <tr> 
+                      <th> Nome </th>
+                      <th> Cep</th>
+                      <th> TELEFONE</th>
+                      <th> </th>
+                      <th> </th>
+                      <th> </th>               
+                                  
+                  </tr>
+                  </thead>
+                  `
+
+           );
+           return html;
+    }
+   
+   
+    function montarDados(pessoas)
+    {
+           var tbody = '<tbody>'
+           var tr = ''
+           $.each(pessoas,function(idx, pessoa)
+           {
+               tr += (
+                     `
+                     
+                     <tr> 
+                            <th scope=row>  ${pessoa.id}  </th>
+                            <td>  ${pessoa.nome} </td>
+                            <td>  ${pessoa.email}. </td>
+                            <td>  ${pessoa.phone} </td>
+                            <td>  ${pessoa.cep} </td>
+                            <td> . ${pessoa.gender} </td>
+                     <td>" . $pessoa["type"]. "</td>
+                            <td> . $pessoa["satus"]. </td> 
+                     </tr>
+                     
+                     `
+   
+              );
+              return html    
+           }
+           )
+            
+          
+    }
 });
--- views/siteResp.php
+++ views/siteResp.php
@@ -1,3 +1,30 @@
+<?php
+ $dadosPessoa = 
+ [
+     [
+         "id" => 10,
+         "nome" => "Aviaozinho",
+         "satus" => "Ativo",
+         "email" => "aviaozinho@curso.com",
+         "phone" => "54 991065280",
+         "gender" => "M",
+         "type" => "CPF",
+         "cep" => "97705760",
+     ],
+     [
+         "id" => 5,
+         "nome" => "Barquinho",
+         "satus" => "Ativo",
+         "email" => "aviaozinho@curso.com",
+         "phone" => "54 991065280",
+         "gender" => "M",
+         "type" => "CPF",
+         "cep" => "97705760",
+     ]
+ ];
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,7 +34,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">  
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/pessoa.js"></script>
+    <script src="./js/pessoa.js"></script>
 
     <title>Site Responsivo</title>
 
@@ -102,7 +129,7 @@
             </div>
             <div class="modal-footer">
               <button type="button" class="btnExcluir" data-bs-dismiss="modal" id="excluir">Excluir</button>
-              <button type="button" class="btnEditar" onclick="envia()" value=5>Salvar</button>
+              <button type="button" class="btnSalvar" onclick="envia()" value=10>Salvar</button>
                
             </div>
           </div>
@@ -116,55 +143,70 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <?php
+               foreach($dadosPessoa as $pessoa)
+               {
+                $html=  "<tr>
+                    <th scope=row>" . $pessoa["id"] . "</th>
+                    <td>" . $pessoa["nome"]. "</td>
+                    <td>" . $pessoa["email"]. "</td>
+                    <td>" . $pessoa["phone"]. "</td>
+                    <td>" . $pessoa["cep"]. "</td>
+                    <td>" . $pessoa["gender"]. "</td>
+                   <td>" . $pessoa["type"]. "</td>
+                    <td>" . $pessoa["satus"]. "</td>
+                    <td>
+                         <button class=btnEditar value=" . $pessoa["id"] ."> EDITAR </button>
+                         <button class=btnExcluir value=" . $pessoa["id"] . ">EXCLUIR </button>  
+                    </td>
+                    
+                </tr>";
+                echo $html;
+                
+               }
+              ;
               
-            </tr>
-            <tr>
-              
-            </tr>
-            <tr>
-              
-            </tr>
+            ?>  
+            
           </tbody>
         </table>
       </table>
 
 <script>
         
-    function envia()
-        {
-              var i = "Arthur"
-              let nome = document.getElementById('nome').value
-              let telefone = document.getElementById('tel').value
-             let cep = document.getElementById('cep').value
-              let email = document.getElementById('email').value
-              let social = document.getElementById('social').value
-             let sexo = document.getElementById('sexo').value
-              let profissao = document.getElementById('profissao').value
-            let cnpj = document.getElementById('cnpj').value
-             var tabela = document.getElementById('tabela')              
-              var row = tabela.insertRow(0)
-              var cel1 = row.insertCell(-1)
-              var cel2 = row.insertCell(-1)
-              var cel3 = row.insertCell()
-              var cel4 = row.insertCell()
-              var cel5 = row.insertCell()
-              var cel6 = row.insertCell()
-              var cel7 = row.insertCell()
-              var cel8 = row.insertCell()   
-             var cel9 = row.insertCell()         
-              cel1.innerHTML += nome 
-              cel2.innerHTML += telefone
-              cel3.innerHTML += cep
-             cel4.innerHTML += email 
-              cel5.innerHTML += social
-              cel6.innerHTML += sexo
-              cel7.innerHTML += profissao
-              cel8.innerHTML += cnpj  
-              cel9.innerHTML += "<button id='botao' class='btn btn-outline-success' value='2'> DELETAR  </button>"
-              cel9.innerHTML += "<button id= botao2 class='btn btn-outline-warning'> EDITAR </button>" 
-             var botao = document.getElementById('botao')
-             var botao2 = document.getElementById('botao2')
+    // function envia()
+    //     {
+    //           var i = "Arthur"
+    //           let nome = document.getElementById('nome').value
+    //           let telefone = document.getElementById('tel').value
+    //          let cep = document.getElementById('cep').value
+    //           let email = document.getElementById('email').value
+    //           let social = document.getElementById('social').value
+    //          let sexo = document.getElementById('sexo').value
+    //           let profissao = document.getElementById('profissao').value
+    //         let cnpj = document.getElementById('cnpj').value
+    //          var tabela = document.getElementById('tabela')              
+    //           var row = tabela.insertRow(0)
+    //           var cel1 = row.insertCell(-1)
+    //           var cel2 = row.insertCell(-1)
+    //           var cel3 = row.insertCell()
+    //           var cel4 = row.insertCell()
+    //           var cel5 = row.insertCell()
+    //           var cel6 = row.insertCell()
+    //           var cel7 = row.insertCell()
+    //           var cel8 = row.insertCell()   
+    //          var cel9 = row.insertCell()         
+    //           cel1.innerHTML += nome 
+    //           cel2.innerHTML += telefone
+    //           cel3.innerHTML += cep
+    //          cel4.innerHTML  += email 
+    //           cel5.innerHTML += social
+    //           cel6.innerHTML += sexo
+    //           cel7.innerHTML += profissao
+    //           cel8.innerHTML += cnpj  
+    //           cel9.innerHTML += "<button id='btnExcluir' class='btn btn-outline-success' value='2'> DELETAR  </button>"
+    //           cel9.innerHTML += "<button id='btnEdit' class='btn btn-outline-warning' > EDITAR </button>" 
+               
               //botao.addEventListener('click', function(){
                      //document.getElementById('tabela').deleteRow(0)
 
@@ -174,9 +216,13 @@
               //botao2.addEventListener('click', function()
              //{
             //})
-      }
-          
+                    
+       
 
+
+
+          
+          
         
 
 
